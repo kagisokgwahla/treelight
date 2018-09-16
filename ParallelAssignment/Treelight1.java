2218012f2b4eb256536688c9cc3356b0c0189802
Kagiso <kagisokgwahla@icloud.com>
2018-09-16 03:14:49 +0200

    Saved Changes

--- ParallelAssignment/Treelight1.java
+++ ParallelAssignment/Treelight1.java
@@ -10,86 +10,100 @@ public class Treelight1{
 			file = new Scanner(new FileInputStream("theinput.txt"));
 		}
 		catch (Exception e){ System.out.println("Not Found");}
-		List<Integer> dimensions = new ArrayList<Integer>();
-		while(file.hasNextInt()){
-			int i = file.nextInt();
-			dimensions.add(i);
-			}
-		file.nextLine();
-		List<Double> AreaSunlight = new ArrayList<Double>();
-		while(file.hasNextInt()){
-			double u = file.nextDouble();
-			AreaSunlight.add(u);
-			}
-	    int numtrees = Integer.valueOf(file.nextLine());
-		int [] trees = new int[numtrees];
-		int counttree = 0;
 		
-		while(file.hasNextLine()){
-			int sum = 0;
-			List<Integer> currenttree = new ArrayList<Integer>();
-		    while(file.hasNextInt()){
-			int i = file.nextInt();
-			currenttree.add(i);
+		List<Integer> dimensions = new ArrayList<Integer>();
+		String [] stringdi = file.nextLine().split("\\s+");
+		for(int x=0; x < stringdi.length; x++){
+			int inputD = Integer.valueOf(stringdi[x]);
+			dimensions.add(inputD);
 			}
+		List<Float> AreaSunlight = new ArrayList<Float>();
+		String [] listnum = file.nextLine().split("\\s+");
+		for(int y=0; y<listnum.length;y++){
+			float inputA = Float.parseFloat(listnum[y]);
+			AreaSunlight.add(inputA);
+			}
+	    int numtree = Integer.valueOf(file.nextLine());
+	    List<Float> Tree = new ArrayList<Float>();
+	    
+	    //Begin looping over the different tress
+	    
+	    while(file.hasNextLine()){
+			float sum = 0;
+			String [] ct = file.nextLine().split("\\s+");
+			List<Integer> currenttree = new ArrayList<Integer>();
+			for(int z=0; z<ct.length ; z++){
+				int inputC = Integer.valueOf(ct[z]);
+				currenttree.add(inputC);
+				}
 			int x = currenttree.get(1);
 			int y = currenttree.get(0);
 			int spread = currenttree.get(2);
-			int startingpoint = x + y*(dimensions.get(1));
+			int startingp = x + y*(dimensions.get(0));
 			
+			//start calculating the trees
+			//This is for the x axis
 			int x_dimension = dimensions.get(1);
 			int y_dimension = dimensions.get(0);
-		    int numx = x_dimension - x;
-		    int x_fact = numx - spread;
-		    int numy = y_dimension - y;
-		    int y_fact = numy -spread;
-		    
-		    if(y_fact>0){
-				for(int x3=0; x3<(spread*5) ; x3+=5){
-					if (x_fact>0){
-						startingpoint= startingpoint + x3;
-						for(int y3=0; y3<spread; y3++){
-							sum+=AreaSunlight.get(startingpoint +y3);
+			int numx = x_dimension - x;
+			int x_fact = numx - spread;
+			//This is for the y axis
+			int numy = y_dimension - y;
+			int y_fact = numy - spread;
+			
+			if(y_fact>0){
+				for( int k=0; k< spread; k++){
+					if(x_fact>0){
+					int sp = startingp+(x_dimension*k);
+						
+						for(int w=0; w< spread ; w++){
+							sum+=AreaSunlight.get(sp+w);
 							}
 						}
-					if (x_fact<0){
-						startingpoint= startingpoint + x_dimension;
-						for(int z=0; z<numx; z++){
-							sum+=AreaSunlight.get(startingpoint +z);
+					if(x_fact<0){
+					int sp1 = startingp+(x_dimension*k);
+						
+						for(int w1=0; w1< numx ; w1++){
+							sum+=AreaSunlight.get(sp1+w1);
 							}
 						}
 					}
-				
+				Tree.add(sum);
 				}
 			if(y_fact<0){
-				for(int x2=0; x2<(numy*5) ; x2+=5){
-					if (x_fact>0){
-						startingpoint= startingpoint + x2;
-						for(int y1=0; y1<spread; y1++){
-							sum+=AreaSunlight.get(startingpoint +y1);
+				for( int k=0; k< numy; k++){
+					if(x_fact>0){
+					int sp2 = startingp+(x_dimension*k);
+						
+						for(int w=0; w< spread ; w++){
+							sum+=AreaSunlight.get(sp2+w);
 							}
+						
 						}
-					if (x_fact<0){
-						startingpoint= startingpoint + x_dimension;
-						for(int x1=0; x1<numx; x1++){
-							sum+=AreaSunlight.get(startingpoint +x1);
+					if(x_fact<0){
+					int sp1 = startingp+(x_dimension*k);
+						
+						for(int w1=0; w1< numx ; w1++){
+							sum+=AreaSunlight.get(sp1+w1);
 							}
+						
 						}
 					}
-			      }
-			trees[counttree]=sum;
-			counttree++;
+				Tree.add(sum);
+				}
+				
 			}
-			int total=0;
-		for(int x=0; x<trees.length; x++){
-			total +=trees[x];
-			}
-		double average = total/numtrees;
-		System.out.println(average);
-		System.out.println(numtrees);
+			float total=0;
+			for(int e=0; e<Tree.size(); e++){
+				total += Tree.get(e);
+				}
+			float average = total/numtree;
+			System.out.println(average);
+			System.out.println(numtree);
+			for(int r=0; r<Tree.size(); r++){
+				System.out.println(Tree.get(r));
+				}
 		
-		for(int x= 0; x<trees.length; x++){
-			System.out.println(trees[x]);
-			}
+		
 		}
 	}
